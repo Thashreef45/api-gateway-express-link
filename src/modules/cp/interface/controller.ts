56877dd415a92882f982ab002747fbc766a57d99
Thashreef45 <ahmedthashreef@gmail.com>
2023-10-16 21:50:35 +0530

    get-cp-recieved-fdms fucntionality completed

--- src/modules/cp/interface/controller.ts
+++ src/modules/cp/interface/controller.ts
@@ -386,6 +386,31 @@ export default {
         } catch (error) {
 
         }
+    },
+
+    getRecievedFdm : (req:Request,res:Response) => {
+        try {
+            client.cpAuth({ token: req.headers.token }, (err: ServiceError, response: AuthRes) => {
+                if (err) {
+                    console.log(err)
+                    res.status(500).json({ error: 'An internal server error occurred.' });
+                    return
+                }
+                if (response.status != 200) res.status(response.status).json(response)
+                else {
+                    consignmentClient.getCpRecievedFdms({token:req.headers.token}, (err: ServiceError, response: any) => {
+                        if (err) {
+                            console.log(err)
+                            res.status(500).json({ error: 'An internal server error occurred.' });
+                            return
+                        }
+                        res.status(response.status).json(response)
+                    })
+                }
+            })
+        } catch (error) {
+            
+        }
     }
 
 
